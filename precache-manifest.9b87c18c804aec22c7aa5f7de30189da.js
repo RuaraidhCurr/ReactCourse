ace79dce29205426c2608d2c8762e7318c8d16c6
Ruaraidh Curran <36444784+RuaraidhCurr@users.noreply.github.com>
2020-02-20 19:21:02 +0000

    Updates

---
+++ precache-manifest.9b87c18c804aec22c7aa5f7de30189da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c993aca213b22f63822529c8041351e5",
+    "url": "/ReactCourse/index.html"
+  },
+  {
+    "revision": "31e091e8e31cc1f1381d",
+    "url": "/ReactCourse/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3b6c50bbf927c716b23c",
+    "url": "/ReactCourse/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "31e091e8e31cc1f1381d",
+    "url": "/ReactCourse/static/js/2.1c90f26d.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/ReactCourse/static/js/2.1c90f26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b6c50bbf927c716b23c",
+    "url": "/ReactCourse/static/js/main.12062b90.chunk.js"
+  },
+  {
+    "revision": "ed4ad604bd24dccd6022",
+    "url": "/ReactCourse/static/js/runtime-main.0304ffde.js"
+  }
+]);
